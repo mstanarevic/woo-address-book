5ad6aad24207f092e69c5bce972ae08df311506e
mstanarevic <mstanarevic@gmail.com>
2019-05-31 03:05:19 +0200

    Added address type to js methods
    Fixed css bugs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,13 +23,15 @@
 			$(this).closest('.wc-address-book-address').addClass('blockUI blockOverlay wc-updating');
 
 			var name = $(this).attr('id');
+			var type = $(this).data('type');
 
 			$.ajax({
 				url: wc_address_book.ajax_url,
 				type: 'post',
 				data: {
 					action: 'wc_address_book_delete',
-					name: name
+					name: name,
+					type: type
 				},
 				success: function (response) {
 					$('.wc-updating').remove();
@@ -45,6 +47,7 @@
 			e.preventDefault();
 
 			var name = $(this).attr('id');
+			var type = $(this).data('type');
 			var primary_address = $('.thc-account-content .woocommerce-Address address');
 			var alt_address = $(this).parent().siblings('address');
 
@@ -63,7 +66,8 @@
 				type: 'post',
 				data: {
 					action: 'wc_address_book_make_primary',
-					name: name
+					name: name,
+					type: type
 				},
 				success: function (response) {
 					$('.wc-updating').removeClass('blockUI blockOverlay wc-updating');
@@ -74,9 +78,9 @@
 		/*
 		 * AJAX call display address on checkout when selected.
 		 */
-		function shipping_checkout_field_prepop() {
+		function address_book_checkout_field_prepop(type) {
 
-			var that = $('#address_book_field #address_book');
+			var that = $('#'+type+'_address_book_field #'+type+'_address_book');
 			var name = $(that).val();
 
 			if (name !== undefined) {
@@ -84,34 +88,41 @@
 				if ('add_new' == name) {
 
 					// Clear values when adding a new address.
-					$('.shipping_address input').not($('#shipping_country')).each(function () {
-						$(this).val('');
-					});
+					if(type === 'billing') {
+						$('.thcommerce-billing-fields input').not($('#'+type+'_country')).each(function () {
+							$(this).val('');
+						});
+					} else if(type === 'shipping') {
+						$('.'+type+'_address input').not($('#'+type+'_country')).each(function () {
+							$(this).val('');
+						});
+					}
 
 					// Set Country Dropdown.
 					// Don't reset the value if only one country is available to choose.
-					if (typeof $('#shipping_country').attr('readonly') == 'undefined') {
-						$('#shipping_country').val('').change();
-						$("#shipping_country_chosen").find('span').html('');
+					if (typeof $('#'+type+'_country').attr('readonly') == 'undefined') {
+						$('#'+type+'_country').val('').change();
+						$('#'+type+'_country_chosen').find('span').html('');
 					}
 
 					// Set state dropdown.
-					$('#shipping_state').val('').change();
-					$("#shipping_state_chosen").find('span').html('');
+					$('#'+type+'_state').val('').change();
+					$('#'+type+'_state_chosen').find('span').html('');
 
 					return;
 				}
 
 				if (name.length > 0) {
 
-					$(that).closest('.shipping_address').addClass('blockUI blockOverlay wc-updating');
+					$(that).closest('.'+type+'_address').addClass('blockUI blockOverlay wc-updating');
 
 					$.ajax({
 						url: wc_address_book.ajax_url,
 						type: 'post',
 						data: {
 							action: 'wc_address_book_checkout_update',
-							name: name
+							name: name,
+							type: type
 						},
 						dataType: 'json',
 						success: function (response) {
@@ -122,16 +133,27 @@
 							});
 
 							// Set Country Dropdown.
-							$('#shipping_country').val(response.shipping_country).change();
-							$("#shipping_country_chosen").find('span').html(response.shipping_country_text);
 
-							// Set state dropdown.
-							$('#shipping_state').val(response.shipping_state);
-							var stateName = $('#shipping_state option[value="' + response.shipping_state + '"]').text();
-							$("#s2id_shipping_state").find('.select2-chosen').html(stateName).parent().removeClass('select2-default');
+							if(type === 'shipping') {
+								$('#shipping_country').val(response.shipping_country).change();
+								$('#shipping_country_chosen').find('span').html(response.shipping_country_text);
+
+								// Set state dropdown.
+								$('#shipping_state').val(response.shipping_state);
+								var stateName = $('#shipping_state option[value="' + response.shipping_state + '"]').text();
+							} else if(type === 'billing') {
+								$('#billing_country').val(response.billing_country).change();
+								$('#billing_country_chosen').find('span').html(response.billing_country_text);
+
+								// Set state dropdown.
+								$('#billing_state').val(response.billing_state);
+								var stateName = $('#billing_state option[value="' + response.billing_state + '"]').text();
+							}
+
+							$("#s2id_"+type+"_state").find('.select2-chosen').html(stateName).parent().removeClass('select2-default');
 
 							// Remove loading screen.
-							$('.shipping_address').removeClass('blockUI blockOverlay wc-updating');
+							$('.'+type+'_address').removeClass('blockUI blockOverlay wc-updating');
 
 						}
 					});
@@ -140,11 +162,17 @@
 			}
 		}
 
-		shipping_checkout_field_prepop();
+		address_book_checkout_field_prepop('shipping');
+		address_book_checkout_field_prepop('billing');
 
-		$('#address_book_field #address_book').change(function () {
-			shipping_checkout_field_prepop();
+		$('#shipping_address_book_field #shipping_address_book').change(function () {
+			address_book_checkout_field_prepop('shipping');
 		});
+
+		$('#billing_address_book_field #billing_address_book').change(function () {
+			address_book_checkout_field_prepop('billing');
+		});
+
 	});
 
 })(window, jQuery);
